631591b36212dcb9dbb03dee9e15f1bc6ecd3a4b
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-05-29 20:06:03 +0530

    article page update

--- src/news/News.js
+++ src/news/News.js
@@ -1,56 +1,129 @@
 import React from 'react';
 import NewsCard from "./NewsCard";
 import ReactDOM from "react-dom"
-function Row(props){
-    return(
+import Loader from "../Loader";
+function Row(props) {
+    return (
         <div className="row">
-                <div className="col-md">{props.res1}</div>
-                <div className="col-md">{props.res2}</div>
-                <div className="col-md">{props.res3}</div>
-            </div>
+            <div className="col-md">{props.res1}</div>
+            <div className="col-md">{props.res2}</div>
+            <div className="col-md">{props.res3}</div>
+        </div>
     )
 }
 
-class News extends React.Component{
-    constructor(props){
+class News extends React.Component {
+    constructor(props) {
         super(props);
-        
+        this.state={
+            result:<Loader/>,data:[]
+        }
     }
-    fetchResults(){
-        fetch("https://newsapi.org/v2/everything?q=nasa&sortBy=popularity&apiKey=26d43b327b1249dba705d6e195af471b")
-        .then(response=>response.json())
-        .then(res=>{
-            console.log(res.articles);
-            const results=[];
-            
-            for(var i=0;i<res.articles.length;){
-                try{
-                    const res1=<NewsCard desc={res.articles[i].title} title={res.articles[i].description} src={res.articles[i].urlToImage}/>;
-                    const res2=<NewsCard desc={res.articles[i+1].title} title={res.articles[i+1].description} src={res.articles[i+1].urlToImage}/>;
-                    const res3=<NewsCard desc={res.articles[i+2].title} title={res.articles[i+2].description} src={res.articles[i+2].urlToImage}/>;
-                    i=i+3;
-                 results.push(<Row res1={res1} res2={res2} res3={res3} />);
-                }
-                catch(err0){
-                    break;
-                    console.log(err0)
-                }
+    fetchResults() {
+        var we = { url: 'https://api.bing.microsoft.com/v7.0/news/search?category=ScienceAndTechnology&q=moon+mars+nasa+space&count=100&freshness=Week' };
+        fetch('http://localhost:8080/newsPost', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(we),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                data=(this.sortArrayByDate(data.value));
+                const filteredData=[];
+                data.forEach(element => {
+                    if(element.category!=="ScienceAndTechnology"){
+                        console.log(element);
+                    }
+                    else
+                        filteredData.push(element);
+                });
+                const dataToBeRendered=[];
                 
-            }
-            ReactDOM.render(results,document.getElementById("divREs"));
-        }).catch(function(error){
-            console.log(error);
-        });
+               console.log(filteredData);
+               for(var i=0;i<filteredData.length;){
+                   const row=(filteredData.slice(i,i+3));
+                   console.log(i,row);console.log(this.createNewsCard(row[0]));
+                    const rowData1=row[0]!==undefined?this.createNewsCard(row[0]):"";
+                    const rowData2=row[1]!==undefined?this.createNewsCard(row[1]):"";
+                    const rowData3=row[2]!==undefined?this.createNewsCard(row[2]):"";
+                    
+                    dataToBeRendered.push(<Row res1={rowData1} res2={rowData2} res3={rowData3}/>);
+                    i=i+3;
+                    //console.log(i,row);
+               }
+                console.log("Asd")
+                //console.log(filteredData);
+                //data=filteredData;
+                //this.setState({result:dataToBeRendered,data:filteredData});
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
-    componentDidMount(){
+    componentDidMount() {
         this.fetchResults();
     }
-    render(){
-        return(
+    render() {
+        return (
             <div id="divREs" className="container">
+                <br/>
+                <h5>Latest Articles</h5>
+                <br/>
+                {this.state.result}
+
+
+                <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                    One of three columns
+                    </div>
+                    <div class="col-sm">
+                    One of three columns
+                    </div>
+                    <div class="col-sm">
+                    One of three columns
+                    </div>
+                    
+                </div>
+                </div>
+
 
             </div>
         )
+    }
+    createNewsCard(element){
+        let imgSrc;
+        if(element.image===undefined){
+            if(element.provider!==undefined){
+                imgSrc=element.provider[0].image.thumbnail.contentUrl;
+            }else
+                imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJzZ1ZlaUsgfXMJMm7XMlqnoRn14OVTD6aXA&usqp=CAU"
+        }
+        else imgSrc=element.image.thumbnail.contentUrl;
+        imgSrc=imgSrc===undefined?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJzZ1ZlaUsgfXMJMm7XMlqnoRn14OVTD6aXA&usqp=CAU":imgSrc;
+
+        let description=element.description;
+        let title=element.name;
+        let articleUrl=element.url;
+        console.log(title,articleUrl,description,imgSrc);
+        return <NewsCard title={title} articleUrl={articleUrl} desc={description} src={imgSrc}/>
+    }
+    sortArrayByDate(objs){
+        function compare( a, b ) {
+            if ( a.datePublished < b.datePublished ){
+              return 1;
+            }
+            if ( a.datePublished > b.datePublished ){
+              return -1;
+            }
+            return 0;
+          }
+          
+         objs= objs.sort( compare );
+         return objs;
     }
 }
 
--- src/news/NewsCard.js
+++ src/news/NewsCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NewsCard(props){
     return(
         <div className="card container-fluid" id="news_card" style={{borderColor:"white"},{borderStyle:"block"}}>
-            <h4 >{props.desc}</h4>
+            <h4 >{props.title}</h4>
             <a target="_blank" id="news_link" href=""> <img className="card-img-top img-fluid" id="news_image"
              src={props.src} alt="Card image cap"/> </a>
             
@@ -13,7 +13,7 @@ function NewsCard(props){
                 <p              
                 id="news_description" >
                     
-                    {props.title}
+                    {props.desc}
                     
                 </p>
             </div>
@@ -22,28 +22,5 @@ function NewsCard(props){
         </div>
     )
 }
-//  style={{width: "100%",whiteSpace: "nowrap",overflow: "hidden",textOverflow:"ellipsis",backgroundColor:"#131316"}}
-
-
-var apiKey="26d43b327b1249dba705d6e195af471b";
-var url = "https://newsapi.org/v2/everything?q=nasa&from=2021-05-24&to=2021-05-24&sortBy=popularity&apiKey=" + apiKey; 
-    var req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.send();
-    req.addEventListener("load", function(){
-      if(req.status == 200 && req.readyState == 4){
-        var response = JSON.parse(req.responseText);
-        var a=6;
-        var description=response.articles[a].description;
-        var title=response.articles[a].title;
-        var image=response.articles[a].urlToImage;
-        var url=response.articles[a].url;
-        
-        
-
-      }
-    });
-
-
 
 export default NewsCard;
