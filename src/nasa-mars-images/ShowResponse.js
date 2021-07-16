711a0a082f80672e7cbf201ed3925f52258f4e91
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-16 16:40:48 +0530

    Update ShowResponse.js

--- src/nasa-mars-images/ShowResponse.js
+++ src/nasa-mars-images/ShowResponse.js
@@ -1,51 +1,67 @@
 import ReactDOM from "react-dom";
-import {LoadRemaining,Image} from "./MarsImages"
+import { LoadRemaining, Image } from "./MarsImages"
 
-function showResponse(response,date,rover){
-	if(response["photos"].length==0){
-		if(date==""){
-		 ReactDOM.render(<i>Invalid date</i>,document.getElementById("divForResults"));
+function showResponse(response, date, rover) {
+	console.log(response);
+
+	if (response["photos"].length == 0) {
+		if (date === "") {
+			ReactDOM.render(<i>Invalid date</i>, document.getElementById("divForResults"));
 		}
 		else
-		   ReactDOM.render(<i>No images taken by {rover} on {date}</i>,document.getElementById("divForResults")); 	
-   } 
-	else{
-		var arr=[];
-		var maxNoOfImgs=0;
-		var Mainres=[];
-		var idList=[];
-		var las=0;
-		var las2=0;
-		for(var a=0;a<response["photos"].length;a++){
-		 maxNoOfImgs++;las=a;
-		 if(maxNoOfImgs==50){
-			 idList.push(info1);
-			 var info1=a+1-maxNoOfImgs+"-"+a;
-			 Mainres.push(<LoadRemaining imgList={arr} info={info1} key={info1}/>)
-			 maxNoOfImgs=0;
-			 arr=[];
-			 las2=a;
-		 }
+			ReactDOM.render(<i>No images taken by {rover} on {date}</i>, document.getElementById("divForResults"));
+	}
+	else {
+		var arr = [];
+		var maxNoOfImgs = 0;
+		var Mainres = [];
+		var idList = [];
+		var las = 0;
+		var las2 = 0;
 
-		 const date=response["photos"][a]["camera"]["full_name"];
-					 
-		 const imgUrl=response["photos"][a]["img_src"];
-		 //document.getElementById("divForResults").appendChild(<Image/>);
-		 //console.log(data+" "+title+" "+imgUrl); 
-		 arr.push(<Image url={imgUrl} date={date} key={a}/>);
-	 }
-	 //document.getElementById("divForResults").innerHTML="<a style='display:none'>asda</a>";
-	 
-	 if(las<50){
-		 Mainres.push(<LoadRemaining imgList={arr} info={0+"-"+las} key={las}/>);
-	 }
-	 else if(arr.length!=0){
-		 Mainres.push(<LoadRemaining imgList={arr} info={las2+"-"+las} key={las}/>);
-	 }
-	 
-	 ReactDOM.render(Mainres,document.getElementById("divForResults"));
-	 //document.getElementById("divForResults").onload=function(){alert('ad');}   	
+		var availableCameras = {};
+
+		for (var a = 0; a < response["photos"].length; a++) {
+
+			if(availableCameras[response.photos[a].camera.full_name]===undefined){
+				availableCameras[response.photos[a].camera.full_name]=[];
+				availableCameras[response.photos[a].camera.full_name].push(response["photos"][a]);
+			}
+			else
+			availableCameras[response.photos[a].camera.full_name].push(response["photos"][a]);
+
+			maxNoOfImgs++; las = a;
+			if (maxNoOfImgs == 50) {
+				idList.push(info1);
+				var info1 = a + 1 - maxNoOfImgs + "-" + a;
+				Mainres.push(<LoadRemaining imgList={arr} info={info1} key={info1} />)
+				maxNoOfImgs = 0;
+				arr = [];
+				las2 = a;
+			}
+
+			const date = response["photos"][a]["camera"]["full_name"];
+
+			const imgUrl = response["photos"][a]["img_src"];
+			//document.getElementById("divForResults").appendChild(<Image/>);
+			//console.log(data+" "+title+" "+imgUrl); 
+			arr.push(<Image url={imgUrl} date={date} key={a} />);
+		}
+		//document.getElementById("divForResults").innerHTML="<a style='display:none'>asda</a>";
+
+		if (las < 50) {
+			Mainres.push(<LoadRemaining imgList={arr} info={0 + "-" + las} key={las} />);
+		}
+		else if (arr.length != 0) {
+			Mainres.push(<LoadRemaining imgList={arr} info={las2 + "-" + las} key={las} />);
+		}
+
+		ReactDOM.render(Mainres, document.getElementById("divForResults"));
+		//document.getElementById("divForResults").onload=function(){alert('ad');}  
+		
+		
+		console.log(availableCameras);
 	}
 }
 
-export {showResponse};
+export { showResponse };
