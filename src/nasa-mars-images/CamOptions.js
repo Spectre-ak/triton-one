47381a09ec222b0a0ec40f8954c766e714d744ca
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-28 11:57:52 +0530

    Create CamOptions.js

---
+++ src/nasa-mars-images/CamOptions.js
@@ -0,0 +1,21 @@
+React = require("react");
+
+class CamOptions extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default CamOptions;
