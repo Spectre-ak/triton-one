40b392520d186fae060f19ba53643bc36bcedb75
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-28 13:44:01 +0530

    Update CamOptions.js

--- src/nasa-mars-images/CamOptions.js
+++ src/nasa-mars-images/CamOptions.js
@@ -1,4 +1,4 @@
-React = require("react");
+import React from "react";
 
 class CamOptions extends React.Component{
     constructor(props){
@@ -6,16 +6,42 @@ class CamOptions extends React.Component{
     }
 
     componentDidMount(){
+        console.log(this.props);
+        const cams_arr=[];
+        for(var key in this.props.ops){
+            console.log(key);
+            if(key==="All")
+                cams_arr.push(
+                    <li className="nav-item" style={{ paddingLeft: "5px" }}>
+                        <a className="nav-link active" id="1" data-toggle="tab" href="#">key</a>
+                    </li>
+                )
+            else
+                cams_arr.push(
+                    <li className="nav-item" style={{ paddingLeft: "5px" }}>
+                        <a className="nav-link" id="1" data-toggle="tab" href="#">key</a>
+                    </li>
+                )
 
+        }
     }
 
     render(){
         return(
-            <React.Fragment>
+            <ul className="nav nav-pills justify-content-center" >
+					<li className="nav-item" style={{ paddingLeft: "5px" }}>
+						<a className="nav-link active" id="1" data-toggle="tab" href="#" onClick={this.curo}>Curiosity (18 Aug 2012 - present)</a>
+					</li>
+					<li className="nav-item" style={{ paddingRight: "5px" }}>
+						<a className="nav-link" id="3" data-toggle="tab" href="#" onClick={this.oppr}>Opportunity (26 Jan 2004 - 11 Jun 2018)</a>
+					</li>
 
-            </React.Fragment>
+					<li className="nav-item" style={{ paddingLeft: "5px", paddingRight: "5px" }}>
+						<a className="nav-link" id="2" data-toggle="tab" href="#" onClick={this.spirit}>Spirit (29 Jan 2004 - 22 Mar 2010)</a>
+					</li>
+			</ul>
         )
     }
-}
+}   
 
 export default CamOptions;
