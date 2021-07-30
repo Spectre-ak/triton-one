3469bf52b7b524f62c98ec2ed723e8f1677ba3c2
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-30 20:53:51 +0530

    Update Loader.js

--- src/Loader.js
+++ src/Loader.js
@@ -33,4 +33,21 @@ function Loader(){
        
     )
 }
+
+
+function LoaderWithoutTimer(){
+    return(
+        <div>
+             <div class="text-center">
+                <div class="spinner-border text-primary" style={{width: "4rem", height: "4rem"}} role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        </div>
+       
+    )
+}
+
+export {LoaderWithoutTimer};
+
 export default Loader;
