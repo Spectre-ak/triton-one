450c687648377f43e47d17cdb8b3962eea02d134
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-09-29 21:47:12 +0530

    Update Image.js

--- src/hubble/Image.js
+++ src/hubble/Image.js
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
-
+import React, { useState, useEffect } from 'react';
 
 
 function ImageComponent(props){
+    const [image_url,update_image_url]=useState(0);
     useEffect(()=>{
-
+        <
     });
     return(
         <>
-            <img className="img-fluid" src={this.state.Url} controls onLoad={()=>{}}/><br/>
+            <img className="img-fluid" src={image_url} controls onLoad={()=>{}} alt={props.data.title}/><br/>
         </>
     )
 }
+
+
+export default ImageComponent;
