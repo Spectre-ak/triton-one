8b947f399e3ccb28e385fe3b559ef27a868bcf32
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-09-30 22:17:40 +0530

    Update Image.js

--- src/hubble/Image.js
+++ src/hubble/Image.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 function ImageComponent(props){
     const [image_url,update_image_url]=useState(0);
     useEffect(()=>{
-        <
+        
     });
     return(
         <>
